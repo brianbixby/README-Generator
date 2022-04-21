1630cec4ad6308cc60c7b28f604c13e07f1359e6
BRIAN BIXBY <BRIANBIXBY0@GMAIL.COM>
2022-04-21 15:19:07 -0700

    added async await and util promisify for fs

--- index.js
+++ index.js
@@ -1,20 +1,15 @@
 "use strict";
 
-// TODO: Include packages needed for this application
 const fs = require("fs");
+const util = require("util");
 const inquirer = require("inquirer");
+
 const generateMarkdown = require("./utils/generateMarkdown.js");
 
-// TODO: Create an array of questions for user input
+const appendFilePromise = util.promisify(fs.appendFile);
+const writeFilePromise = util.promisify(fs.writeFile);
+
 const questions = [
-	{
-		type: 'input',
-		message: 'What is the TITLE of your project?',
-		name: 'title',
-		when(answers) {
-			writeToFile("README-test.md", `# ${answers.title}`);
-		},
-	},
 	{
 		type: 'input',
 		message: 'Please DESCRIBE your project.',
@@ -48,87 +43,116 @@ function writeToFile(fileName, data) {
 		if (err) {
 			throw (err);
 		}
-		console.log("Success!");
+		// console.log("Success!");
 	})
 }
 
-// TODO: Create a function to initialize app
-function init() {
-	inquirer
-		.prompt(questions)
-		.then(data => {
-			writeToFile("README-test.md", generateMarkdown(data));
-		})
-		.catch((error) => {
-			if (error.isTtyError) {
-				// Prompt couldn't be rendered in the current environment
-			} else {
-				// Something else went wrong
-			}
-		});
-
-
-
-	// {
-	// 	type: 'checkbox',
-	// 	message: 'Select toppings',
-	// 	name: 'toppings',
-	// 	choices: [
-	// 	  new inquirer.Separator(' = The Meats = '),
-	// 	  {
-	// 		name: 'Pepperoni',
-	// 	  },
-	// 	  {
-	// 		name: 'Ham',
-	// 	  },
-	// 	  {
-	// 		name: 'Ground Meat',
-	// 	  },
-	// 	  {
-	// 		name: 'Bacon',
-	// 	  },
-	// 	  new inquirer.Separator(' = The Cheeses = '),
-	// 	  {
-	// 		name: 'Mozzarella',
-	// 		checked: true,
-	// 	  },
-	// 	  {
-	// 		name: 'Cheddar',
-	// 	  },
-	// 	  {
-	// 		name: 'Parmesan',
-	// 	  },
-	// 	  new inquirer.Separator(' = The usual ='),
-	// 	  {
-	// 		name: 'Mushroom',
-	// 	  },
-	// 	  {
-	// 		name: 'Tomato',
-	// 	  },
-	// 	  new inquirer.Separator(' = The extras = '),
-	// 	  {
-	// 		name: 'Pineapple',
-	// 	  },
-	// 	  {
-	// 		name: 'Olives',
-	// 		disabled: 'out of stock',
-	// 	  },
-	// 	  {
-	// 		name: 'Extra cheese',
-	// 	  },
-	// 	],
-	// 	validate(answer) {
-	// 	  if (answer.length < 1) {
-	// 		return 'You must choose at least one topping.';
-	// 	  }
-
-	// 	  return true;
-	// 	},
-	//   }
+async function init() {
+	try {
+		const { title } = await inquirer.prompt([{ type: 'input', message: 'What is the TITLE of your project?', name: 'title' }]);
+		writeFilePromise("README.md", `# ${title}
+	  
+`);
+		const { description, installationInstructions, usageInformation, contributionGuidelines, testInstructions } = await inquirer.prompt(questions);
+		appendFilePromise("README.md", `## Description
+		
+${description}
+	
+## Installation
+	
+${installationInstructions}
+	
+## Usage
+	
+${usageInformation}
+	
+## Contributing
+	
+${contributionGuidelines}
+	
+## Tests
+	
+${testInstructions}
+	
+## Table of Contents
+	
+${description}
+	
+## License
+	
+${description}
+	
+## Questions
+	
+${description}
+`);
+	}
+	catch (err) {
+		if (err.isTtyError) console.log("Prompt couldn't be rendered in the current environment");
+		else console.log(err);
+	}
 }
 
 // Function call to initialize app
 init();
+
+
+// {
+// 	type: 'checkbox',
+// 	message: 'Select toppings',
+// 	name: 'toppings',
+// 	choices: [
+// 	  new inquirer.Separator(' = The Meats = '),
+// 	  {
+// 		name: 'Pepperoni',
+// 	  },
+// 	  {
+// 		name: 'Ham',
+// 	  },
+// 	  {
+// 		name: 'Ground Meat',
+// 	  },
+// 	  {
+// 		name: 'Bacon',
+// 	  },
+// 	  new inquirer.Separator(' = The Cheeses = '),
+// 	  {
+// 		name: 'Mozzarella',
+// 		checked: true,
+// 	  },
+// 	  {
+// 		name: 'Cheddar',
+// 	  },
+// 	  {
+// 		name: 'Parmesan',
+// 	  },
+// 	  new inquirer.Separator(' = The usual ='),
+// 	  {
+// 		name: 'Mushroom',
+// 	  },
+// 	  {
+// 		name: 'Tomato',
+// 	  },
+// 	  new inquirer.Separator(' = The extras = '),
+// 	  {
+// 		name: 'Pineapple',
+// 	  },
+// 	  {
+// 		name: 'Olives',
+// 		disabled: 'out of stock',
+// 	  },
+// 	  {
+// 		name: 'Extra cheese',
+// 	  },
+// 	],
+// 	validate(answer) {
+// 	  if (answer.length < 1) {
+// 		return 'You must choose at least one topping.';
+// 	  }
+
+// 	  return true;
+// 	},
+//   }
 
 
 // fs.writeFile('log.txt', JSON.stringify(response, null, 2), (err) => {
