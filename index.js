f1e01cabe3a62fc048262afb4855f23494e3dd97
BRIAN BIXBY <BRIANBIXBY0@GMAIL.COM>
2022-04-21 18:27:20 -0700

    refactored code and added license section

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ const inquirer = require("inquirer");
 
 const generateMarkdown = require("./utils/generateMarkdown.js");
 
-const appendFilePromise = util.promisify(fs.appendFile);
-const writeFilePromise = util.promisify(fs.writeFile);
-
 const questions = [
+	{
+		type: 'input',
+		message: 'What is the TITLE of your project?',
+		name: 'title'
+	},
 	{
 		type: 'input',
 		message: 'Please DESCRIBE your project.',
@@ -34,58 +36,42 @@ const questions = [
 		type: 'input',
 		message: 'Please include any TEST INSTRUCTIONS for your project.',
 		name: 'testInstructions'
+	},
+	{
+		type: 'list',
+		message: 'Please select a LICENSE for this project.',
+		name: 'license',
+		choices: ['Apache License 2.0', 'GNU GPLv3', 'ISC', 'MIT', 'None'],
+	},
+	{
+		type: 'input',
+		message: 'Please enter your GITHUB USERNAME.',
+		name: 'github'
+	},
+	{
+		type: 'input',
+		message: 'Please enter your EMAIL ADDRESS.',
+		name: 'email'
 	}
 ];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-	fs.appendFile(fileName, data, err => {
-		if (err) {
-			throw (err);
-		}
-		// console.log("Success!");
-	})
+async function writeToFile(fileName, data) {
+	try {
+		console.log("data: ", data);
+		const writeFilePromise = util.promisify(fs.writeFile)
+		await writeFilePromise(fileName, data);
+	}
+	catch (err) {
+		console.log("Write File Error");
+		return err;
+	}
 }
 
 async function init() {
 	try {
-		const { title } = await inquirer.prompt([{ type: 'input', message: 'What is the TITLE of your project?', name: 'title' }]);
-		writeFilePromise("README.md", `# ${title}
-	  
-`);
-		const { description, installationInstructions, usageInformation, contributionGuidelines, testInstructions } = await inquirer.prompt(questions);
-		appendFilePromise("README.md", `## Description
-		
-${description}
-	
-## Installation
-	
-${installationInstructions}
-	
-## Usage
-	
-${usageInformation}
-	
-## Contributing
-	
-${contributionGuidelines}
-	
-## Tests
-	
-${testInstructions}
-	
-## Table of Contents
-	
-${description}
-	
-## License
-	
-${description}
-	
-## Questions
-	
-${description}
-`);
+		const data = await inquirer.prompt(questions);
+		const markdown = generateMarkdown(data);
+		await writeToFile("README.md", markdown);
 	}
 	catch (err) {
 		if (err.isTtyError) console.log("Prompt couldn't be rendered in the current environment");
@@ -95,69 +81,3 @@ ${description}
 
 // Function call to initialize app
 init();
-
-
-// {
-// 	type: 'checkbox',
-// 	message: 'Select toppings',
-// 	name: 'toppings',
-// 	choices: [
-// 	  new inquirer.Separator(' = The Meats = '),
-// 	  {
-// 		name: 'Pepperoni',
-// 	  },
-// 	  {
-// 		name: 'Ham',
-// 	  },
-// 	  {
-// 		name: 'Ground Meat',
-// 	  },
-// 	  {
-// 		name: 'Bacon',
-// 	  },
-// 	  new inquirer.Separator(' = The Cheeses = '),
-// 	  {
-// 		name: 'Mozzarella',
-// 		checked: true,
-// 	  },
-// 	  {
-// 		name: 'Cheddar',
-// 	  },
-// 	  {
-// 		name: 'Parmesan',
-// 	  },
-// 	  new inquirer.Separator(' = The usual ='),
-// 	  {
-// 		name: 'Mushroom',
-// 	  },
-// 	  {
-// 		name: 'Tomato',
-// 	  },
-// 	  new inquirer.Separator(' = The extras = '),
-// 	  {
-// 		name: 'Pineapple',
-// 	  },
-// 	  {
-// 		name: 'Olives',
-// 		disabled: 'out of stock',
-// 	  },
-// 	  {
-// 		name: 'Extra cheese',
-// 	  },
-// 	],
-// 	validate(answer) {
-// 	  if (answer.length < 1) {
-// 		return 'You must choose at least one topping.';
-// 	  }
-
-// 	  return true;
-// 	},
-//   }
-
-
-// fs.writeFile('log.txt', JSON.stringify(response, null, 2), (err) => {
-// 	if (err) {
-// 		throw (err);
-// 	}
-// 	console.log('Success!');
-// })
